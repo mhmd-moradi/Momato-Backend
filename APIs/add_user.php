c35e545740f22afdbb36ac3ed73882d51d720691
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-05-30 22:08:24 +0300

    Added retrieve inserted user id and send back response

--- APIs/add_user.php
+++ APIs/add_user.php
@@ -17,4 +17,29 @@ $query = $mysqli->prepare("INSERT INTO Users(username, password,email, role,firs
 $query->bind_param("ssssss", $username, $password, $email, $fname, $lname, $gender);
 $query->execute();
 
+
+$response = [];
+if($mysqli->affected_rows === 1)
+    {
+        //retrieve inserted user id
+        $query = $mysqli->prepare("Select user_id from Users where username = ?");
+        $query->bind_param("s", $username);
+        $query->execute();
+        $query->store_result();
+        $num_rows = $query->num_rows;
+        $query->bind_result($id);
+        $query->fetch();
+        if($num_rows == 0)
+            $response["user_id"] = -1;
+        else
+            //send inserted user id to front end
+            $response["user_id"] = $id;
+                
+    }
+else
+    $response["user_id"] = -1;
+
+$json = json_encode($response);
+echo $json;
+
 ?>
