8238d37c0bfb27c6a8932b171022d1930e6a0257
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-05-31 07:49:57 +0300

    Updated get_reviews api to include getting reviews for one restaurant

--- APIs/get_reviews.php
+++ APIs/get_reviews.php
@@ -1,16 +1,23 @@
 <?php
 include("connection.php");
 
-$query = $mysqli->prepare("SELECT * from Reviews");
+if(isset($_GET["restaurant_id"])){
+    $restaurant_id = $_GET["restaurant_id"];
+    $query = $mysqli->prepare("SELECT * from Reviews where restaurant_id = ? ");
+    $query->bind_param("s", $restaurant_id);
+}else
+    $query = $mysqli->prepare("SELECT * from Reviews");
+    
 $query->execute();
 $array = $query->get_result();
 
 $response = [];
-while($todo = $array->fetch_assoc()){
-    $response[] = $todo;
+while($restaurant = $array->fetch_assoc()){
+    $response[] = $restaurant;
 } 
 
 $json = json_encode($response);
 echo $json;
+
 
 ?>
