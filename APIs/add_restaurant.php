088f3ad73f6bfee4445b490f1b77c2626154309d
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-05-31 03:20:06 +0300

    Added sending is_admin in the json response

---
+++ APIs/add_restaurant.php
@@ -0,0 +1,30 @@
+<?php
+
+include("connection.php");
+if(isset($_POST["image"]) && $_POST["name"]  && $_POST["location"]  && $_POST["category"]  && $_POST["opening_time"]  && $_POST["closing_time"]  && $_POST["user"] ){
+    $image = $_POST["image"];
+    $name = $_POST["name"];
+    $location = $_POST["location"];
+    $category = $_POST["category"];
+    $opening_time = $_POST["opening_time"];
+    $closing_time = $_POST["closing_time"];
+    $user = $_POST["user"];
+}else
+    die("Failure!");
+
+
+$query = $mysqli->prepare("INSERT INTO Restaurants(type, created_by,restaurant_name, location ,image, opening_time, closing_time) VALUES (?, ?, ?, ?, ?, ?, ?)");
+$query->bind_param("iisssss", $category, $user, $name, $location, $image, $opening_time, $closing_time);
+$query->execute();
+
+
+$response = [];
+if($mysqli->affected_rows === 1)
+    $response["success"] = true;
+else
+    $response["success"] = false;
+
+$json = json_encode($response);
+echo $json;
+
+?>
--- APIs/login.php
+++ APIs/login.php
@@ -9,19 +9,24 @@ if(isset($_POST["username"]) && isset($_POST["password"])){
     die("Failure!");
 
 //retrieve inserted user id
-$query = $mysqli->prepare("Select user_id from Users where username = ? and password = ?");
+$query = $mysqli->prepare("Select user_id, role from Users where username = ? and password = ?");
 $query->bind_param("ss", $username, $password);
 $query->execute();
 $query->store_result();
 $num_rows = $query->num_rows;
-$query->bind_result($id);
+$query->bind_result($id, $role);
 $query->fetch();
 
 if($num_rows == 0)
     $response["user_id"] = -1;
-else
-    //send inserted user id to front end
+else{
     $response["user_id"] = $id;
+    if($role == 1)
+        $response["is_admin"] = true;
+    else    
+        $response["is_admin"] = false;
+}
+    
 
 $json = json_encode($response);
 echo $json;
