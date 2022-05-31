3f77f5166cb059d84b74dd69fa2a5e8258f3ef97
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-05-31 20:35:08 +0300

    Added accept_review api

---
+++ APIs/accept_review.php
@@ -0,0 +1,27 @@
+<?php
+
+include("connection.php");
+
+if(isset($_GET["review_id"])){
+    $review_id = $_GET["review_id"];
+    
+}else
+    die("Failure!");
+
+$query = $mysqli->prepare("UPDATE Reviews SET status = 2 WHERE review_id = ?");
+$query->bind_param("s", $review_id);
+$query->execute();
+
+
+$response = [];
+
+if($mysqli->affected_rows === 1)
+    $response["success"] = true;
+
+else
+    $response["success"] = false;
+
+$json = json_encode($response);
+echo $json;
+
+?>
