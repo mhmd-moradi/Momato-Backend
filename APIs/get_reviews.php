0de900156976bbae288247f616d58edaa8242441
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-05-31 15:23:54 +0300

    Removed user_id from get_reviews api

--- APIs/get_reviews.php
+++ APIs/get_reviews.php
@@ -1,11 +1,10 @@
 <?php
 include("connection.php");
 
-if(isset($_GET["restaurant_id"]) && isset($_GET["user_id"])){
+if(isset($_GET["restaurant_id"]) ){
     $restaurant_id = $_GET["restaurant_id"];
-    $user_id = $_GET["user_id"];
-    $query = $mysqli->prepare("SELECT * from Reviews, Users where restaurant_id = ? and Users.user_id = Reviews.user_id and Reviews.user_id = ?");
-    $query->bind_param("ss", $restaurant_id, $user_id);
+    $query = $mysqli->prepare("SELECT * from Reviews, Users where restaurant_id = ? and Users.user_id = Reviews.user_id");
+    $query->bind_param("s", $restaurant_id);
 }else
     $query = $mysqli->prepare("SELECT * from Reviews, Users where Users.user_id = Reviews.user_id");
     
